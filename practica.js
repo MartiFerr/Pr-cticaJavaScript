7eafa5095523a1c5095da0465d997629df14e6cd
MartiFerr <136343935+MartiFerr@users.noreply.github.com>
2023-08-04 18:34:45 -0300

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,24 @@
+
+console.log (250+20)
+console.log (500-200)
+console.log (20 * 4)
+console.log (60 / 2)
+let nombredelasuma = 'La suma es de 250+20'
+let nombredelaresta = 'La  resta es de 500-200'
+let nombredelamultiplicación = 'La multiplicación es de 20 * 4'
+let nombredeladivision= 'La división es de 60 / 2'
+alert ('La suma de la variable -nombre de la suma- es de 270')
+alert ('La suma de la variable -nombre de la resta- es de 300')
+alert ('La suma de la variable -nombre de la multiplicación- es de 80')
+alert ('La suma de la variable -nombre de la división- es de 30')
+alert('Mi edad es 14 años')
+alert('Mi fecha de nacimiento es el 27/09/2008')
+alert('¿Como es su nombre compañero?,Bienvenido a nuestro curso 3 año')
+let nombrealumno = 'Martina'
+let apelliodoalumno = 'Ferrando'
+console.log ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
+alert ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
+let diez = (5+5)
+let tres = (2+1)
+let resultadoMultiplicación = (10 * 3)
+console.log (10 * 3)
