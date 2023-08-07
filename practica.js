895e0d80f532baeaafac4a2d6187082f71283176
MartiFerr <136343935+MartiFerr@users.noreply.github.com>
2023-08-07 18:46:24 -0300

    Cambio

--- practica.js
+++ practica.js
@@ -1,27 +1,24 @@
 
-console.log ('La suma es'+(250+20))
-console.log ('La resta es'+ (500-200))
-console.log  ('La es multiplicación'+ (20 * 4))
-console.log ('La es división'+ (60 / 2))
-let suma = 250+20
-let resta = 500-200
-let multiplicación = 20 * 4
-let division= 60 / 2
-alert ('La suma de la variable es' + suma )
-alert ('La resta de la variable es' + resta)
-alert ('La multiplicación de la variable es'+ multiplicación)
-alert ('La division de la variable es' + division)
-alert('Mi fecha de nacimiento es el 27/09/2008')
-let nombrealumno
-let apelliodoalumno = 'Ferrando'
-console.log ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
+console.log ('La suma es' + (250+20))
+console.log ('La resta es' + (500-200))
+console.log  ('La es multiplicación' + (20 * 4))
+console.log ('La es división' + (60 / 2))
+let suma =  (250+20)
+let resta =  (500-200)
+let multiplicación =  (20 * 4)
+let division= (60 / 2)
+alert ('La suma de la variable es ' + suma )
+alert ('La resta de la variable es ' + resta)
+alert ('La multiplicación de la variable es '+ multiplicación)
+alert ('La division de la variable es ' + division)
+console.log ('Bienvenido a nuestro curso 3 año, Mi nombre es Martina Ferrando')
 alert ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
+ let nombrealumno = prompt ('¿Como es su nombre compañero?')
+let apellidoalumno = prompt('¿Como es su apellido compañero?')
+alert ('Bienvenido a nuestro curso 3 año ' +  nombrealumno + apellidoalumno)
+let fechanacimientoalumno = prompt ('¿Como es su fecha de nacimiento?')
+alert ('Tu edad es'+ (2023-fechanacimientoalumno))
 let diez = (5+5)
 let tres = (2+1)
-let resultadoMultiplicación = (10 * 3)
-console.log (10 * 3)
-nombrealumno = prompt ('¿Como es su nombre compañero?')
-alert ('Bienvenido a nuestro curso 3 año'+ nombrealumno)
-let fechanacimientoalumno = prompt ('¿Como es su fecha de nacimiento?')
-alert ('Fecha de nacimiento'+ fechanacimientoalumno)
-
+let resultadomultiplicacion = ( diez * tres)
+alert ( 'Resultado multiplicación ' + resultadomultiplicacion)
