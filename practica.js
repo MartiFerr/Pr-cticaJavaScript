4d866423d72cc89d4c677cc6ed4584169768fea5
alumno <alumno@Instituto.local>
2023-08-07 10:54:24 -0300

    cambio

--- practica.js
+++ practica.js
@@ -1,20 +1,18 @@
 
-console.log (250+20)
-console.log (500-200)
-console.log (20 * 4)
-console.log (60 / 2)
-let nombredelasuma = 'La suma es de 250+20'
-let nombredelaresta = 'La  resta es de 500-200'
-let nombredelamultiplicación = 'La multiplicación es de 20 * 4'
-let nombredeladivision= 'La división es de 60 / 2'
-alert ('La suma de la variable -nombre de la suma- es de 270')
-alert ('La suma de la variable -nombre de la resta- es de 300')
-alert ('La suma de la variable -nombre de la multiplicación- es de 80')
-alert ('La suma de la variable -nombre de la división- es de 30')
-alert('Mi edad es 14 años')
+console.log ('La suma es'+(250+20))
+console.log ('La resta es'+ (500-200))
+console.log  ('La es multiplicación'+ (20 * 4))
+console.log ('La es división'+ (60 / 2))
+let suma = 250+20
+let resta = 500-200
+let multiplicación = 20 * 4
+let division= 60 / 2
+alert ('La suma de la variable es' + suma )
+alert ('La resta de la variable es' + resta)
+alert ('La multiplicación de la variable es'+ multiplicación)
+alert ('La division de la variable es' + division)
 alert('Mi fecha de nacimiento es el 27/09/2008')
-alert('¿Como es su nombre compañero?,Bienvenido a nuestro curso 3 año')
-let nombrealumno = 'Martina'
+let nombrealumno
 let apelliodoalumno = 'Ferrando'
 console.log ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
 alert ('Bienvenido a nuestro curso 3 año,Mi nombre es Martina Ferrando')
@@ -22,3 +20,8 @@ let diez = (5+5)
 let tres = (2+1)
 let resultadoMultiplicación = (10 * 3)
 console.log (10 * 3)
+nombrealumno = prompt ('¿Como es su nombre compañero?')
+alert ('Bienvenido a nuestro curso 3 año'+ nombrealumno)
+let fechanacimientoalumno = prompt ('¿Como es su fecha de nacimiento?')
+alert ('Fecha de nacimiento'+ fechanacimientoalumno)
+
